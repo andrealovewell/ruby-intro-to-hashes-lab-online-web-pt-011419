64a33625f8a8697794df2e2890ef1ed9b33fade1
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-29 03:36:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,13 +61,6 @@ def monopoly_with_fourth_tier
 		{
 			:pieces =>4,
 
-			:names =>
-			{
-				:reading_railroad =>{"mortgage_value" =>"$100"},
-				:pennsylvania_railroad =>{"mortgage_value" =>"$200"},
-				:b_and_o_railroad =>{"mortgage_value" =>"$400"},
-				:shortline => {"mortgage_value" =>"$800"}
-			},
 			:rent_in_dollars =>
 			{
 				:one_piece_owned =>25,
@@ -75,6 +68,15 @@ def monopoly_with_fourth_tier
 				:three_pieces_owned =>100,
 				:four_pieces_owned => 200
 			}
+
+			:names =>
+			{
+				:reading_railroad =>{"mortgage_value" =>"$100"},
+				:pennsylvania_railroad =>{"mortgage_value" =>"$200"},
+				:b_and_o_railroad =>{"mortgage_value" =>"$400"},
+				:shortline => {"mortgage_value" =>"$800"}
+			},
+			
 		}
 	}
 
